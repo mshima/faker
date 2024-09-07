4d33b3160ddda91c5e594a84b624f8fd6af2e69c
Marcelo Shima <marceloshima@gmail.com>
2024-09-07 17:36:37 -0300

    Update faker.spec.ts

--- test/faker.spec.ts
+++ test/faker.spec.ts
@@ -20,7 +20,7 @@ describe('faker', () => {
 
     const file: unknown = await import('..');
     expect(file).toBeDefined();
-    expect(file.Faker).toBe(Faker);
+    expect(file.foo).toBe(Faker);
 
     new Faker({ locale: { metadata: { title: '' } } });
 
